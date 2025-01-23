281a014a0e4034623ff720fee9cae9a28a9c7165
Christopher Makousky <christopher.makousky@gmail.com>
2025-01-23 13:19:20 -0600

    Completed the client-side mutations.ts and queries.ts files. The login mutation is fully implemented, but none of the others are working yet.

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -11,3 +11,43 @@ mutation Login($email: String!, $password: String!) {
   }
 }
 `;
+
+export const CREATE_USER = gql `
+mutation CreateUser($input: NewUser!) {
+  createUser(input: $input) {
+    user {
+      username
+      email
+      password
+    }
+  }
+}
+`;
+
+export const DELETE_BOOK = gql `
+mutation DeleteBook($bookId: ID!) {
+  deleteBook(bookId: $bookId) {
+    title
+    link
+    image
+    description
+    bookId
+    authors
+    _id
+  }
+}
+`;
+
+export const ADD_BOOK = gql `
+mutation SaveBook($input: AddBook!) {
+  saveBook(input: $input) {
+    title
+    link
+    image
+    description
+    bookId
+    authors
+    _id
+  }
+}
+`;
--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -1,1 +1,20 @@
 import { gql } from '@apollo/client';
+
+export const GET_SINGLE_USER = gql `
+query GetSingleUser($id: String!, $username: String) {
+  getSingleUser(_id: $id, username: $username) {
+    username
+    savedBooks {
+      title
+      link
+      image
+      description
+      bookId
+      authors
+      _id
+    }
+    email
+    bookCount
+  }
+}
+`;
